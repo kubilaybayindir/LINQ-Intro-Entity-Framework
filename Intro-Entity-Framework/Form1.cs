8103524afb77aee66267fc51c1a654bf882fa576
Kubilay BAYINDIR <45577585+bayindirkubilay60@users.noreply.github.com>
2023-05-28 10:30:14 +0300

    Added LINQ Query with Extension Methods

--- Intro-Entity-Framework/Form1.cs
+++ Intro-Entity-Framework/Form1.cs
@@ -17,60 +17,84 @@ namespace Intro_Entity_Framework
         public string[] GenerateName()
         {
             string[] names =new string[30];
-
-
+                        
             for (int i = 0; i < 30; i++)
             {
-                names[i]=NameData.GetFirstName();
+                names[i] = NameData.GetFirstName();
             }
-
             return names;
         }
-
+        private void BtGetAllNames_Click(object sender, EventArgs e)
+        {
+            //listBox1.DataSource = null;
+            listBox1.DataSource = GenerateName().ToList();
+        }
         private void BtSixLetter_Click(object sender, EventArgs e)
         {
             listBox1.DataSource = null;
-            IEnumerable<string> query = from name in GenerateName()
-                                        where name.Length == 6
-                                        orderby name
-                                        select name.ToUpper();
+
+            //LINQ QUERY
+            //IEnumerable<string> query = from name in GenerateName()
+            //                            where name.Length == 6
+            //                            orderby name
+            //                            select name.ToUpper();
+            //listBox1.DataSource = query.ToList();
+
+            //LINQ QUERY WITH EXTENSION MEDTHODS
+            IEnumerable<string> query = GenerateName().Where(name => name.Length == 6).Select(name => name.ToUpper());
             listBox1.DataSource = query.ToList();
         }
 
         private void BtAz_Click(object sender, EventArgs e)
         {
             listBox1.DataSource = null;
-            IEnumerable<string> query = from name in GenerateName()
-                                        orderby name ascending
-                                        select name.ToUpper();
+            //IEnumerable<string> query = from name in GenerateName()
+            //                            orderby name ascending
+            //                            select name.ToUpper();
+            //listBox1.DataSource = query.ToList();
+
+
+            IEnumerable<string> query = GenerateName().OrderBy(name => name);
             listBox1.DataSource = query.ToList();
         }
 
         private void BtZa_Click(object sender, EventArgs e)
         {
             listBox1.DataSource = null;
-            IEnumerable<string> query = from name in GenerateName()
-                                        orderby name descending
-                                        select name.ToUpper();
+            //IEnumerable<string> query = from name in GenerateName()
+            //                            orderby name descending
+            //                            select name.ToUpper();
+            //listBox1.DataSource = query.ToList();
+
+            IEnumerable<string> query = GenerateName().OrderByDescending(name => name);
             listBox1.DataSource = query.ToList();
+
+
         }
 
         private void BtALetter_Click(object sender, EventArgs e)
         {
             listBox1.DataSource = null;
-            IEnumerable<string> query = from name in GenerateName()
-                                        where name.ToUpper().StartsWith("A")
-                                        select name.ToUpper();
-            listBox1.DataSource=query.ToList();
+            //IEnumerable<string> query = from name in GenerateName()
+            //                            where name.ToUpper().StartsWith("A")
+            //                            select name.ToUpper();
+            //listBox1.DataSource=query.ToList();
+
+            IEnumerable<string> query = GenerateName().Where(name => name.StartsWith("A")).Select(n => n.ToUpper());
+            listBox1.DataSource = query.ToList();
+
         }
 
         private void BtRLetter_Click(object sender, EventArgs e)
         {
             listBox1.DataSource = null;
-            IEnumerable<string> query = from name in GenerateName()
-                                            where name.ToUpper().EndsWith("R")
-                                            orderby name ascending
-                                        select name.ToUpper();
+            //IEnumerable<string> query = from name in GenerateName()
+            //                            where name.ToUpper().EndsWith("R")
+            //                            orderby name ascending
+            //                            select name.ToUpper();
+            //listBox1.DataSource = query.ToList();
+
+            IEnumerable<string> query = GenerateName().Where(name => name.ToUpper().EndsWith("R")).OrderBy(na => na).Select(n => n.ToUpper());
             listBox1.DataSource = query.ToList();
         }
 
@@ -89,12 +113,6 @@ namespace Intro_Entity_Framework
                 }
             }
             listBox1.Items.Add(longest);
-        }
-
-        private void BtGetAllNames_Click(object sender, EventArgs e)
-        {
-            listBox1.DataSource = null;
-            listBox1.DataSource = GenerateName().ToList();
         }
     }
 }
